c570691ebd4539c58353e588cf2820355903f413
Malik Ali Raza <melicalyraza5@gmail.com>
2026-08-03 18:44:51 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+//#include <string>
+#include <map>
+
+using namespace std;
+
+class Record {
+protected:
+    string date;
+
+public:
+    Record(const string& d) : date(d) {}
+    virtual void display() const = 0; 
+    string getDate() const { return date; }
+    virtual ~Record() {} 
+};
